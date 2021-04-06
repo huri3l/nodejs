b4a75584c3a233ddd3b19bbf40691fe11d8c6153
Huriel Lopes <huriel-lopes@hotmail.com>
2021-04-05 23:46:44 -0300

    nodets-tests-sql: Migrations, repositories and controllers

--- nodets-tests-sql/src/server.ts
+++ nodets-tests-sql/src/server.ts
@@ -1,11 +1,13 @@
+import "reflect-metadata";
 import express from "express";
+import "./database";
+import { router } from "./routes";
 
 const port = 3333;
 const app = express();
 
-app.get("/users", (req, res) => {
-  return res.send("olar");
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(port, () =>
   console.log(`🎉 API is running on http://localhost:${port}`)
